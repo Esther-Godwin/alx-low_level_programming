f7984557bc63dcdf9211849c08fdf94821156165
Dex <gchidex08@gmail.com>
2023-02-17 13:46:27 -0800

    Write C programs to print strings

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - Prints "Programming is like building a multilingual puzzle
+*
+* Return: 0
+*/
+int main(void)
+{
+	char str[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(str);
+
+	return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - Prints the size of various data types
+*
+* Return: 0
+*/
+int main(void)
+{
+	int char_size = sizeof(char);
+	int int_size = sizeof(int);
+	int long_int_size = sizeof(long int);
+	int  long_long_int_size = sizeof(long long int);
+	int float_size = sizeof(float);
+
+	printf("Size of a char: %d byte(s)\n", char_size);
+	printf("Size of an int: %d byte(s)\n", int_size);
+	printf("Size of a long int: %d byte(s)\n", long_int_size);
+	printf("Size of a long long int: %d byte(s)\n", long_long_int_size);
+	printf("Size of a float: %d byte(s)\n", float_size);
+
+	return (0);
+}
