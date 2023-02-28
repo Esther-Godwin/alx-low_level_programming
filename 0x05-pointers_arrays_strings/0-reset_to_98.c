f8cb2cc6007138f4865de35850c620e998267ad9
Dex <gchidex08@gmail.com>
2023-02-28 02:56:00 -0800

    Add a function that updated an integer value via its pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* reset_to_98 - Updated the value that the pointer argument points to
+*@n: Pointer to an int to be  updated
+*
+* Return: void
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
