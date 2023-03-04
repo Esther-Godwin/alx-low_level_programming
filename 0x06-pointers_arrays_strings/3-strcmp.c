ac29a146ba60c3c079a3a0d1c33d96fc0a652d1e
Dex <cgodwin315@stu.ui.edu.ng>
2023-03-03 21:52:01 -0800

    Make correction to string compare function

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,17 +9,12 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-	int sum1 = 0;
-	int sum2 = 0;
-
-	while (*s1 != '\0')
+	while (*s1 != '\0' && *s2 != '\0')
 	{
-		sum1 += *s1;
+		if (*s1 - *s2 != 0)
+			return (*s1 - *s2);
 		s1++;
+		s2++;
 	}
-	while (*s2 != '\0')
-	{
-		sum2 += *s2;
-	}
-	return (sum1 - sum2);
+	return (0);
 }
