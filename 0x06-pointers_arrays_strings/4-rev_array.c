c3fb5b58dd8088814b43000a2b5b0e5ff7813fed
Dex <cgodwin315@stu.ui.edu.ng>
2023-03-03 21:36:00 -0800

    Add function to reverse array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* reverse_array - Reverse the content of an array of integers
+* @a: An array of integers
+* @n: The numbers of elements in the array
+*
+* Return: void
+*/
+void reverse_array(int *a, int n)
+{
+	char *tmp = a;
+	char *end = a + n - 1;
+
+	while (a != end && n > 0)
+	{
+		*temp = *a;
+		*a = *end;
+		*end = *temp;
+		if (a + 1 == end)
+			break;
+		a++;
+		end--;
+	}
+}
